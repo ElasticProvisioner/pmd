ee2381cb22bba9406b035bda75d846083dd8886d
Peter Paul Bakker <peter.paul.bakker@stokpop.nl>
2026-04-03 14:18:26 +0200

    Revert PmdKotlinParser to throw ParserException in non-test calls to keep behaviour same. Use custom parser that throws ParserException for running tests and getting notified for parse issues.

--- pmd-kotlin/src/main/java/net/sourceforge/pmd/lang/kotlin/ast/PmdKotlinParser.java
+++ pmd-kotlin/src/main/java/net/sourceforge/pmd/lang/kotlin/ast/PmdKotlinParser.java
@@ -4,16 +4,11 @@
 
 package net.sourceforge.pmd.lang.kotlin.ast;
 
-import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Lexer;
-import org.antlr.v4.runtime.RecognitionException;
-import org.antlr.v4.runtime.Recognizer;
 
-import net.sourceforge.pmd.lang.ast.ParseException;
 import net.sourceforge.pmd.lang.ast.impl.antlr4.AntlrBaseParser;
-import net.sourceforge.pmd.lang.document.FileLocation;
 import net.sourceforge.pmd.lang.kotlin.ast.KotlinParser.KtKotlinFile;
 
 /**
@@ -24,13 +19,6 @@ public final class PmdKotlinParser extends AntlrBaseParser<KotlinNode, KtKotlinF
     @Override
     protected KtKotlinFile parse(final Lexer lexer, ParserTask task) {
         KotlinParser parser = new KotlinParser(new CommonTokenStream(lexer));
-        parser.removeErrorListeners();
-        parser.addErrorListener(new BaseErrorListener() {
-            @Override
-            public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e) {
-                throw new ParseException(msg).withLocation(FileLocation.caret(task.getFileId(), line, charPositionInLine));
-            }
-        });
         return parser.kotlinFile().makeAstInfo(task);
     }
 
